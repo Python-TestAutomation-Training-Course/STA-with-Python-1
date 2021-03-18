005fb003eda52516c8b0c10238986405e6f32db8
PETERSBURG\Andrei_Zhidelev <andrei_zhidelev@epam.com>
2021-03-18 13:30:04 +0300

    Lecture 1

---
+++ Lecture_1_Introduction_homework/test_sample.py
@@ -0,0 +1,26 @@
+import pytest
+
+
+def test_pass():
+    """This test should pass"""
+    assert True
+
+
+def get_length(test_string):
+    """Return length of the string"""
+    return None
+
+
+@pytest.mark.parametrize("test_string,result", [("test", 4), ("\t", 1)])
+def test_string_length(test_string, result):
+    assert get_length(test_string) == result
+
+
+def remove_spaces(test_string):
+    """Removes spaces, tabs for the string"""
+    return None
+
+
+@pytest.mark.parametrize("test_string,result", [("test", "test"), ("\ttab", "tab"), ("  whitespace ", "whitespace")])
+def test_string_remove_spaces(test_string, result):
+    assert remove_spaces(test_string) == result
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # STA-with-Python-1
 Lectures and homeworks
+
+Lectures are located in slides folders (Jupyter notebook and html file)
+Homeworks are in homeworks folders
+
+Install the last version of Python
+
+To run jupyter notebook:
+- Create a virtual environment
+- Install dependencies from requirements.txt with command `pip install -r requirements.txt`
+- Run ```jupyter notebook```
+
+Also lecture slide is available in html file.
+
