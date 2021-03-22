f6d79a9dac8c1c84d124faed807a9d7e3d5af5e7
demid-sm <svetlana1potapova@gmail.com>
2021-03-22 19:37:54 +0300

    Fix tests

--- Lecture_1_Introduction_homework/test_sample.py
+++ Lecture_1_Introduction_homework/test_sample.py
@@ -8,7 +8,7 @@ def test_pass():
 
 def get_length(test_string):
     """Return length of the string"""
-    return None
+    return len(test_string)
 
 
 @pytest.mark.parametrize("test_string,result", [("test", 4), ("\t", 1)])
@@ -18,7 +18,7 @@ def test_string_length(test_string, result):
 
 def remove_spaces(test_string):
     """Removes spaces, tabs for the string"""
-    return None
+    return test_string.strip()
 
 
 @pytest.mark.parametrize("test_string,result", [("test", "test"), ("\ttab", "tab"), ("  whitespace ", "whitespace")])
