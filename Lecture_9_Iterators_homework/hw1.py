428305e9c431136247b69d3b5203b2d8784622ef
PETERSBURG\Andrei_Zhidelev <andrei_zhidelev@epam.com>
2021-05-13 13:50:40 +0300

    LEcture 9 and HW

---
+++ Lecture_9_Iterators_homework/hw1.py
@@ -0,0 +1,24 @@
+"""
+Write a function that merges integer from sorted files and returns an iterator
+
+file1.txt:
+1
+3
+5
+
+file2.txt:
+2
+4
+6
+
+>>> list(merge_sorted_files(["file1.txt", "file2.txt"]))
+[1, 2, 3, 4, 5, 6]
+
+Add tests for this function.
+"""
+from pathlib import Path
+from typing import Iterator, List, Union
+
+
+def merge_sorted_files(file_list: List[Union[Path, str], ...]) -> Iterator:
+    pass
